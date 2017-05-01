9d0a35df5891f1eaf1c6af0fa3aa2b2137076a8b
sivashankerN <shankar@vlabs.ac.in>
2017-05-01 10:41:20 +0530

    Update plots for edx machine with run1..run5 tests

--- src/testing/scripts/get-graphs.py
+++ src/testing/scripts/get-graphs.py
@@ -11,13 +11,18 @@ label = sys.argv[2]
 color_name = sys.argv[3]
 graph_title = sys.argv[4]
 fig = plt.figure()
-#image_filename = "./graphs-processes/" + file_name + ".png"
-#image_filename = "./graphs-free-ram/" + file_name + ".png"
-#image_filename = "./graphs-cs/" + file_name + ".png"
-image_filename = "./graphs-idle-cpu/" + file_name + ".png"
+#image_filename = "./graphs-processes/" + file_name + ".png" full version cc
+#image_filename = "./graphs-free-ram/" + file_name + ".png"  full version cc
+#image_filename = "./graphs-cs/" + file_name + ".png"        full version cc
+#image_filename = "./graphs-idle-cpu/" + file_name + ".png"  full version cc
+
+#image_filename = "./split-cc-graphs-processes/" + file_name + ".png"   #edx running machine: split version cc
+#image_filename = "./split-cc-graphs-free-ram/" + file_name + ".png"   #edx running machine: split version cc
+#image_filename = "./split-cc-graphs-cs/" + file_name + ".png"   #edx running machine: split version cc
+image_filename = "./split-cc-graphs-idle-cpu/" + file_name + ".png"   #edx running machine: split version cc
 fig.suptitle(graph_title, fontsize=14, fontweight='bold')
-plt.ylabel(' % of idle cpu ')
-plt.xlabel(' > Time  in seconds')
+plt.ylabel('% idle Cpu')
+plt.xlabel('Time  in seconds')
 
 
 tmp_file_name = ".tmp"
@@ -26,9 +31,11 @@ tmp_file_name = ".tmp"
 #os.system("tail -n +3 " + file_name + "> file.tmp && mv file.tmp " + file_name)
 #os.system("awk '{print $1}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated") # this line for printing number of processes waiting for run time ( r value)
 #os.system("awk '{print $4}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated") # this line for printingfree ram  ( free )
-
 #os.system("awk '{print $12}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated")  # this line is for printing  Context switches ( cs)
-os.system("awk '{print $15}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated")  # this line is for printing cpu utilization ( idle cpu)	
+
+#os.system("awk '{print $1}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated")  # this line is for printing number of processes waiting for runtime)	
+#os.system("awk '{print $2}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated")  # this line is for printing RAM utilization
+os.system("awk '{print $15}' " + file_name + "| tail -n +3" +  ">"  + file_name + ".updated")
 new_file_name = file_name  + ".updated"
 
 with open(new_file_name, "r") as f:
--- src/testing/scripts/vmstats.sh
+++ src/testing/scripts/vmstats.sh
@@ -1,5 +1,15 @@
-python get-graphs.py "test-case2-vmstat-run1.txt" "run1 status" "red" "Run1 : only edx + cluster components running"
-python get-graphs.py "test-case2-vmstat-run2.txt" "run2 status" "cyan" "Run2: edx + cluster components + one lab running"
-python get-graphs.py "test-case2-vmstat-run3.txt" "run3 status" "blue" "Run3: edx + cluster components + one lab running"
-python get-graphs.py "test-case2-vmstat-run4.txt" "run4 status" "green" "Run4: edx + cluster components + one lab running"
-python get-graphs.py "test-case2-vmstat-run5.txt" "run5 status" "blue" "Run5: edx + cluster components + one lab running"
+###un comment following lines to generate graphs for full version of college cloud edition
+
+#python get-graphs.py "test-case2-vmstat-run1.txt" "run1 status" "red" "Run1 : only edx + cluster components running"
+#python get-graphs.py "test-case2-vmstat-run2.txt" "run2 status" "cyan" "Run2: edx + cluster components + one lab running"
+#python get-graphs.py "test-case2-vmstat-run3.txt" "run3 status" "blue" "Run3: edx + cluster components + one lab running"
+#python get-graphs.py "test-case2-vmstat-run4.txt" "run4 status" "green" "Run4: edx + cluster components + one lab running"
+#python get-graphs.py "test-case2-vmstat-run5.txt" "run5 status" "blue" "Run5: edx + cluster components + one lab running"
+
+
+###un comment following lines to generate graphs for edx machine(split version of college cloud edition)
+python get-graphs.py "split-cc-vmstat-edx-run1.txt"  "run1 status" "red" "Run1 : only edx running "
+python get-graphs.py "split-cc-vmstat-edx-run2.txt" "run2 status" "cyan" "Run2: edx running"
+python get-graphs.py "split-cc-vmstat-edx-run3.txt" "run3 status" "blue" "Run3: edx running"
+python get-graphs.py "split-cc-vmstat-edx-run4.txt" "run4 status" "green" "Run4: edx running"
+python get-graphs.py "split-cc-vmstat-edx-run5.txt" "run5 status" "blue" "Run5: edx running"
